1f2fc7f532d005b015236570b8501b3ad5d2b365
dumdoo <rohailfaisal14@gmail.com>
2021-10-27 18:06:07 +0500

    Remove unneccasary imports

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from ftplib import all_errors
 from modules.ftp_wrapper import FTPWrapper
-from ftplib import all_errors as FTP_all_errors
-import sys
 
 if __name__ == "__main__":
     ftp = FTPWrapper("192.168.1.1")
